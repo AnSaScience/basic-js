c8752ec6aba2b11abd382e040ca8320e425fc00b
AnSaScience <ansascience@gmail.com>
2022-04-29 00:14:17 +0300

    feat: finish task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam(names) {
+   if(Array.isArray(names)){
+  let correctNamesArr=names.filter(x=>{
+      return typeof x=="string";})
+    let teamName=correctNamesArr.map(x=>x.trim()[0].toUpperCase()).sort((a,b)=>a>b?1:-1).join("");
+     return teamName;
+ }
+ return false
 }
+
 
 module.exports = {
   createDreamTeam
