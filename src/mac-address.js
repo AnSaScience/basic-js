f2027054fc30cb70fe695a64a56e762bc04c9bd0
AnSaScience <ansascience@gmail.com>
2022-05-01 20:02:54 +0300

    finish few tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
   // remove line with error and write your code here
+  let regexp=new RegExp(/(([A-F]|[0-9]){2}-){5}([A-F]|[0-9]){2}\b/);
+  return n.search(regexp)!==-1;
 }
 module.exports = {
   isMAC48Address
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight( arr ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let filteredAndSortedArr=arr.filter(x=>x!==-1).sort((a,b)=>a>b?1:-1);
+  let sortedByHeight=[];
+  arr.forEach(x=>{
+    if(x===-1){
+      sortedByHeight.push(x);
+    }
+    else{
+      sortedByHeight.push(filteredAndSortedArr.shift())
+    }
+  })
+  return sortedByHeight;
 }
 
 module.exports = {
